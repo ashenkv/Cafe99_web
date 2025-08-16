9d46645869fcf31bc1a83c37e0474a2ee38574c4
Ashen Lalantha <143162341+ashenkv@users.noreply.github.com>
2025-08-16 11:14:00 +0530

    Add files via upload

---
+++ Js/script.js
@@ -0,0 +1,14 @@
+function menuOpen() {
+    document.getElementById("navSm").style.display = "none";
+    document.getElementById("menuItems").style.display = "block";
+}
+
+function menuClose() {
+    document.getElementById("menuItems").style.display = "none";
+    document.getElementById("navSm").style.display = "block";
+}
+
+
+function a(){
+    alert("success");
+}
